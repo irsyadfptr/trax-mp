93f94cd480f8415ce030391961cbafc5c9795512
Irsyad Fakhri Putranto <irsyad.putranto@sg-edts.com>
2022-11-19 21:09:29 +0700

    fix init api and prettier warning

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Any files in API folder gonna be treated as API routes
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
