a6f918b46247b8cb2b09d972f44df2195470f5be
Irsyad Fakhri Putranto <irsyad.putranto@sg-edts.com>
2022-11-21 08:34:57 +0700

    Fix token auth when expired

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -37,11 +37,25 @@ const Playlist = ({playlist}) => {
 }
 
 export const getServerSideProps = async ({query, req}) => {
-    const {id} = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+    let user;
+
+    try {
+        user = await validateToken(req.cookies.TRAX_ACCESS_TOKEN)
+    } catch (error) {
+        return {
+            redirect: {
+                destination: '/signin',
+                permanent: false
+            }
+        }
+    }
+
+
+    // const {id} = validateToken(req.cookies.TRAX_ACCESS_TOKEN)
     const [playlist] = await prisma.playlist.findMany({
         where: {
             id: +query.id,
-            userId: id,
+            userId: user.id,
         },
         include: {
             songs: {
